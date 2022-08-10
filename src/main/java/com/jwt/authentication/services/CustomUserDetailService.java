486923a9f7e5e88bf417315cc7d06241d16ed59a
Subrat <subratnayak0506@gmail.com>
2022-08-10 12:53:16 +0530

    Hide Credentials

--- src/main/java/com/jwt/authentication/services/CustomUserDetailService.java
+++ src/main/java/com/jwt/authentication/services/CustomUserDetailService.java
@@ -2,8 +2,8 @@ package com.jwt.authentication.services;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,12 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailService implements UserDetailsService{
 
+	@Value("${app.username}")
+	private String username;
+	
+	@Value("${app.password}")
+	private String password;
+	
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
-		if(userName.equals("Subrat"))
+		if(userName.equals(username))
 		{
-			return new User("Subrat","Nayak",new ArrayList<>());
+			return new User(username,password,new ArrayList<>());
 		}else
 		{
 			throw new UsernameNotFoundException("User Not Found!");
